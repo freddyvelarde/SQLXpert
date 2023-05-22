9548ded2806771785e49ed2742520712330e72ce
Freddy Velarde <freddyvelarde59@gmail.com>
2023-05-21 22:04:04 -0400

    :zap: Adding `query normalization` function

---
+++ SQLXpertRestApi/utils/queries.normalization.go
@@ -0,0 +1,47 @@
+package utils
+
+func splitString(str string) []string {
+	var splited []string
+	var strPiece string
+
+	for _, char := range str {
+		if char == ' ' || char == '.' || char == ';' || char == ',' {
+			splited = append(splited, strPiece)
+
+			// strPiece = ""
+			strPiece = string(char)
+			splited = append(splited, strPiece)
+
+			strPiece = ""
+		} else {
+			strPiece += string(char)
+		}
+	}
+
+	splited = append(splited, strPiece)
+
+	return splited
+}
+
+func tableMatch(target string, str interface{}) bool {
+	for _, s := range str.([]string) {
+		if s == target {
+			return true
+		}
+	}
+	return false
+}
+
+func NormalizeQuery(query string, tables interface{}) string {
+	querySplited := splitString(query)
+	var res string
+
+	for _, word := range querySplited {
+		if tableMatch(word, tables) {
+			res += "\"" + word + "\""
+		} else {
+			res += word
+		}
+	}
+	return res
+}
