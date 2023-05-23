fea02e9f248e32b7cf2e9e5d3f4ac00352a154b9
Freddy Velarde <freddyvelarde59@gmail.com>
2023-05-22 20:06:21 -0400

    :sparkles: new features

--- SQLXpertRestApi/app.go
+++ SQLXpertRestApi/app.go
@@ -10,7 +10,7 @@ func app() *gin.Engine {
 	app := gin.Default()
 
 	corsConfig := cors.DefaultConfig()
-	corsConfig.AllowOrigins = []string{"http://172.19.0.1:7677", "http://localhost:7677", "http://192.168.0.9:7677"}
+	corsConfig.AllowOrigins = []string{"http://172.19.0.1:7677", "http://localhost:7677"}
 	app.Use(cors.New(corsConfig))
 
 	routes.Router(app)
