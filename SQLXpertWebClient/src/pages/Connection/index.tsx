d05d3c99aa492a379f5c8cc4b5777abf6b0f1089
Freddy Velarde <freddyvelarde59@gmail.com>
2023-05-25 20:36:03 -0400

    :package: adding new pages

--- SQLXpertWebClient/src/pages/Connection/index.tsx
+++ SQLXpertWebClient/src/pages/Connection/index.tsx
@@ -2,14 +2,13 @@ import { useEffect, useState } from "react";
 import Input from "../../components/Input";
 import { useNavigate } from "react-router-dom";
 import { dbConnection } from "../../data/endpoints";
-import { DbConnection } from "../../interfaces/dbConnectionConfig";
+import DbConnection from "../../interfaces/dbConnectionConfig";
 import useHttpRequest from "../../hooks/useHttpRequest";
-
-interface Response {
-  connected: boolean;
-}
+import useDatabases from "../../hooks/useDatabases";
+import { ConnectionResponse } from "../../interfaces/HttpResponses";
 
 export default function Connection() {
+  const { addNewDatabase } = useDatabases();
   const navigate = useNavigate();
 
   // database config
@@ -19,8 +18,9 @@ export default function Connection() {
     dbName: "freddy_db",
     user: "admin",
     password: "admin",
+    workspace: "workspace name",
   });
-  const { data, fetchData } = useHttpRequest<Response>(dbConnection, {
+  const { data, fetchData } = useHttpRequest<ConnectionResponse>(dbConnection, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -34,33 +34,13 @@ export default function Connection() {
     }),
   });
 
-  // Rest Api response
-  // const [response, setResponse] = useState<any>([]);
-
   const navigateToDashboard = () => {
     const res = data?.connected;
     if (res) {
       navigate(`/${dbConfigConnection.dbName}`, { replace: true });
+      console.log(data);
     }
   };
-
-  // const makeQuery = async () => {
-  //   const request = await fetch(dbConnection, {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({
-  //       host: dbConfigConnection.host,
-  //       port: +dbConfigConnection.port,
-  //       user: dbConfigConnection.user,
-  //       password: dbConfigConnection.password,
-  //       dbName: dbConfigConnection.dbName,
-  //     }),
-  //   });
-  //   const response = await request.json();
-  //   setResponse(response);
-  // };
 
   useEffect(() => {
     navigateToDashboard();
@@ -69,12 +49,21 @@ export default function Connection() {
   const handleOnSubmitEvent = (e: React.FormEvent) => {
     e.preventDefault();
     fetchData();
+    addNewDatabase(dbConfigConnection);
   };
 
   return (
     <div>
       <h1>PostgreSql connection</h1>
       <form action="" onSubmit={handleOnSubmitEvent}>
+        <Input
+          type="text"
+          placeholder="Workspace"
+          label="WORKSPACE: "
+          state={dbConfigConnection}
+          name="workspace"
+          setState={setDbConfigConnection}
+        />
         <Input
           type="text"
           placeholder="username"
